78199967b3845505861480d4da93dff2fe1b52f8
Kevin Li <kevin@li.org>
2022-11-16 12:20:19 -0600

    init

---
+++ C101 Factorial [Swift]/.merlin/build/main.swift
@@ -0,0 +1,26 @@
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 8
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+//assisted: David 
+// Add your code below:
+
+func recursion(from target: Int) -> Int{
+    print("Calculating factorial(\(target))...")
+    
+    if target > 1{
+        let calculate = target * recursion(from: target - 1)
+        print("Non-terminal case, returning \(calculate).")
+        return calculate
+    }
+    else{
+        print("Terminal case, returning 1.")
+        return 1
+    }
+}
+
+
+let result = recursion(from: n)
+print("The final result is: \(result).")
